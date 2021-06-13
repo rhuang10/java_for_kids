b88fefb4dcd22e07f49e0b22623d51247c4b2d45
Ruihong Huang <ruihong.huang@gmail.com>
2021-06-13 14:01:13 +0100

    Hello World

---
+++ code/HelloWorld.java
@@ -0,0 +1,8 @@
+package code;
+
+class HelloWorld{        // java uses { ... } to define block,
+                         // while python use ':'
+    public static void main(String[] args) {
+        System.out.println("Hello world");   // ; is required     
+    }
+}
